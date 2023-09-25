0f08a63c47dece1648876b47ae13510116d00cef
syphrose <swabulwenyi@gmail.com>
2023-09-25 09:24:34 -0700

    Puppeting

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# Puppet configuration without password
+
+file_line{'turn off password authentication':
+path => '/etc/ssh/ssh_config',
+line => 'PasswordAuthentication no'
+}
+
+file_line{'declare identity file':
+path => '/etc/ssh/ssh_config',
+line => 'IdentifyFile ~/.ssh/school'
+}
