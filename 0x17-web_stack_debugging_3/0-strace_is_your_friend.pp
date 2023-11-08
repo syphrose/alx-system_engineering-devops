c6ead47cc790db8d57729c7e2d2ce53fb479c03c
syphrose <swabulwenyi@gmail.com>
2023-11-08 03:30:18 -0800

    Use tmux to run strace in one window and curl in another one

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,9 @@
+# A puppet manuscript to find out why Apache is returning a 500 error.
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+#replace line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
+}
