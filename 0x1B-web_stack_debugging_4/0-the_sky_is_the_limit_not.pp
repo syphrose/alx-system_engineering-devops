7003248698076beb2b0d217ada279b7990285ce4
syphrose <swabulwenyi@gmail.com>
2023-11-15 17:43:42 +0300

    Stimulating HTTP requests to a web server.

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Testing how well our web server setup featuring Nginx is doing under pressure.
+
+# Increase the ULIMIT of the default file using ApacheBench
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
